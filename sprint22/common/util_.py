ae14fa6b815b7f04ba281c98795635c1a1c6395c
Hiroki <hiroki.suzaki@gmail.com>
2018-09-24 20:13:38 +0900

    updated

---
+++ sprint22/common/util_.py
@@ -0,0 +1,24 @@
+# coding: utf-8
+import sys
+sys.path.append('..')
+import os
+from common.np import *
+
+#前処理(単語リストから単語IDへ変換)関数
+def preprocess(text):
+    text = text.lower()
+    text = text.replace('.', ' .')
+    words = text.split(' ')
+
+    word_to_id={}
+    id_to_word={}
+    for word in words:
+        if word not in word_to_id:
+            new_id = len(word_to_id)
+            word_to_id[word]=new_id
+            id_to_word[new_id]=word
+
+    corpus = [word_to_id[w] for w in words]
+    corpus = np.array(corpus)
+
+    return corpus, word_to_id, id_to_word
