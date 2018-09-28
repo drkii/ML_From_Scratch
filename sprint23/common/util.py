66d630633ab73996e753d25f396ca56d5c40e8a9
Hiroki <hiroki.suzaki@gmail.com>
2018-09-28 17:32:12 +0900

    updated on 0928

--- sprint23/common/util.py
+++ sprint23/common/util.py
@@ -4,8 +4,29 @@ sys.path.append('..')
 import os
 from common.np import *
 
-
 def preprocess(text):
+    
+    length=len(text)
+    for i in range(length):
+        text=text[i][0]
+        text = text.lower()
+        text = text.replace('.', ' .')
+        words = text.split(' ')
+        
+        word_to_id = {}
+        id_to_word = {}
+        for word in words:
+            if word not in word_to_id:
+                new_id = len(word_to_id)
+                word_to_id[word] = new_id
+                id_to_word[new_id] = word
+    
+        corpus = np.array([word_to_id[w] for w in words])
+        
+    return corpus, word_to_id, id_to_word
+
+
+def preprocess_2(text):
     text = text.lower()
     text = text.replace('.', ' .')
     words = text.split(' ')
@@ -220,7 +241,7 @@ def eval_perplexity(model, corpus, batch_size=10, time_size=35):
         sys.stdout.flush()
 
     print('')
-    ppl = np.exp(total_loss / max_iters)
+    ppl = np.exp(total_loss / max_iters) 
     return ppl
 
 
